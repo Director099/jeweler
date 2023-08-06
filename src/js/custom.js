725c9666ae3cfaaebf2e36482ca0a8d677e88ff0
redx099 <ferz099@mail.ru>
2023-08-06 18:36:36 +0500

    добавлен аккордион

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import "../blocks/components/accordion/accordion.js";
 import '../blocks/components/section-content/section-content.js';
 import '../blocks/project/main-slider/main-slider.js';
 import '../blocks/project/blog-place/blog-place.js';
