15a9e12f12844bd24516e683cfbcf0eda5ffea51
redx099 <ferz099@mail.ru>
2023-08-06 20:07:29 +0500

    добавлен nav-changes-scroll

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import "../blocks/components/accordion/accordion.js";
+import "../blocks/components/navigation-changer/move-to.js";
+import "../blocks/components/navigation-changer/navigation-changer.js";
 import '../blocks/components/section-content/section-content.js';
 import '../blocks/project/main-slider/main-slider.js';
 import '../blocks/project/blog-place/blog-place.js';
